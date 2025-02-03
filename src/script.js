0f0022df04e8af17028587cd17419c80b6d5cb58
Fe-Re <Felixreisenhauer@googlemail.com>
2025-02-04 00:09:36 +0100

    added local time instead of UTC time

--- src/script.js
+++ src/script.js
@@ -5,9 +5,15 @@ const sub5 = document.querySelector(`.sub-5`);
 const reset = document.querySelector(`.reset-timer`);
 const short = document.querySelector(`.short-btn`);
 const long = document.querySelector(`.long-btn`);
-timer.style.setProperty('--timer-label', "'Work'");
-const today = new Date().toISOString().split("T")[0];
 const quote = document.querySelector(`.quote`);
+
+timer.style.setProperty('--timer-label', "'Work'");
+
+const date = new Date();
+const year = date.getFullYear();
+const month = String(date.getMonth() + 1).padStart(2, "0");
+const day = String(date.getDate()).padStart(2, "0");
+const today = `${year}-${month}-${day}`;
 
 let interval;
 let isRunning = false;
